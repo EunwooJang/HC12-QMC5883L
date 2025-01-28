c20c3238485f730943b1a1e54c28f2994029a67e
장은우 <oxygenic314@gmail.com>
2025-01-29 02:31:55 +0900

    Create qmc5883l_master.ino

---
+++ qmc5883l_master/qmc5883l_master.ino
@@ -0,0 +1,36 @@
+#include "qmc5883l_multi.h"
+#include <SoftwareSerial.h>
+
+// HC-12 핀 설정 (RX, TX)
+SoftwareSerial HC12(10, 11); // HC-12 모듈 연결 핀 (TX, RX)
+
+// 슬레이브 개수 설정
+#define SLAVE_AMOUNT 5
+
+// QMC5883LMulti 객체 생성
+QMC5883LMulti compassMulti(SLAVE_AMOUNT);
+
+void setup() {
+  Serial.begin(9600);
+  HC12.begin(9600); // HC-12 통신 시작
+
+  compassMulti.begin(); // QMC5883LMulti 초기화
+}
+
+void loop() {
+  char* data = compassMulti.getAllSensorData();
+
+  // 수집된 데이터를 출력
+  for (int i = 0; i < SLAVE_AMOUNT; i++) {
+    Serial.print("Slave ");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    for (int j = 0; j < 6; j++) {
+      Serial.print(data[i * 6 + j], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+
+  delay(3000); // 3초 대기 후 다음 데이터 요청
+}
