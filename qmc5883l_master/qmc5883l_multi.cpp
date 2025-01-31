9db4ab215599718e2e8a094a6ee2235031cc0a9e
장은우 <oxygenic314@gmail.com>
2025-01-31 14:01:15 +0900

    Update qmc5883l_multi.cpp

--- qmc5883l_master/qmc5883l_multi.cpp
+++ qmc5883l_master/qmc5883l_multi.cpp
@@ -1,8 +1,9 @@
 #include "qmc5883l_multi.h"
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
 // 외부에서 선언된 HC12 참조
-extern SoftwareSerial HC12;
+extern SoftwareSerial hc12;
 
 QMC5883LMulti::QMC5883LMulti(uint8_t slaveAmount)
   : slaveAmount(slaveAmount) {
@@ -10,9 +11,6 @@ QMC5883LMulti::QMC5883LMulti(uint8_t slaveAmount)
   memset(combinedData, 0, 6 * slaveAmount);
 }
 
-void QMC5883LMulti::begin() {
-  // 필요시 추가적인 초기화 코드
-}
 
 char* QMC5883LMulti::getAllSensorData() {
   memset(combinedData, 0, 6 * slaveAmount);
@@ -43,18 +41,18 @@ bool QMC5883LMulti::requestSensorData(uint8_t slaveId, char* buffer) {
   char command[5];
   snprintf(command, 5, "S%dMD", slaveId);
 
-  HC12.write(command);
+  hc12.write(command);
 	
-	if (HC12.available()) {
+	if (hc12.available()) {
   		unsigned long startTime = millis();
-  		while ((millis() - startTime) < 200) { // Wait up to 200ms for response
-    		if (HC12.available() >= 10) {
-      		HC12.readBytes(buffer, 10);
+  		while ((millis() - startTime) < 100) { // Wait up to 200ms for response
+    		if (hc12.available() >= 10) {
+      		hc12.readBytes(buffer, 10);
       		return validateReceivedData(buffer, slaveId);
     		}
   		}
-		while (HC12.avaliable()) {
-			HC12.read();		
+		while (hc12.avaliable()) {
+			hc12.read();		
 		}
 	}
   return false; // Timed out
@@ -64,18 +62,18 @@ bool QMC5883LMulti::requestResendData(uint8_t slaveId, char* buffer) {
   char command[5];
   snprintf(command, 5, "S%dMU", slaveId);
 
-  HC12.write(command);
+  hc12.write(command);
 	
 	if (HC12.available()) {
   		unsigned long startTime = millis();
-  		while ((millis() - startTime) < 200) { // Wait up to 200ms for response
-    		if (HC12.available() >= 10) {
-      		HC12.readBytes(buffer, 10);
+  		while ((millis() - startTime) < 100) { // Wait up to 200ms for response
+    		if (hc12.available() >= 10) {
+      		hc12.readBytes(buffer, 10);
       		return validateReceivedData(buffer, slaveId);
     		}
   		}
-		while (HC12.avaliable()) {
-			HC12.read();		
+		while (hc12.avaliable()) {
+			hc12.read();		
 		}
 	}
   return false; // Timed out
@@ -84,10 +82,5 @@ bool QMC5883LMulti::requestResendData(uint8_t slaveId, char* buffer) {
 bool QMC5883LMulti::validateReceivedData(const char* data, uint8_t slaveId) {
   char expectedHeader[5];
   snprintf(expectedHeader, sizeof(expectedHeader), "D%dMD", slaveId);
-
-  if (strncmp(data, expectedHeader, 4) != 0) {
-    return false; // Header mismatch
-  }
-
-  return true;
+  return strncmp(data, expectedHeader, 4) == 0;
 }
